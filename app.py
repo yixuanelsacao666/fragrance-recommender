1d7769f22bc112c3bd535e621b73c7bc30b177d2
yixuanelsacao666 <yixuanelsacao@gmail.com>
2026-01-12 17:07:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -130,5 +130,9 @@ def index():
                            personality_options=personality_mapping.keys())
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+'''if __name__ == '__main__':
+    app.run(debug=True)'''
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
